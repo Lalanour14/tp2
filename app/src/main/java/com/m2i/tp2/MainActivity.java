f78ca355f21ef8c533da6ce685ace12a38552f51
Salim <bouzidsezlim@gmail.com>
2023-08-30 13:15:26 +0200

    fonction botton

--- app/src/main/java/com/m2i/tp2/MainActivity.java
+++ app/src/main/java/com/m2i/tp2/MainActivity.java
@@ -3,8 +3,17 @@ package com.m2i.tp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Random random = new Random(); // class random => generer nombres aleatoires ...
+    TextView nbr1_txt, nbr2_txt;
+    Button resp1_btn, resp2_btn, resp3_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
