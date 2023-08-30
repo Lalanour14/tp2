0d2f237adca931106699ab750cb3b2a6bfae5eff
Salim <bouzidsezlim@gmail.com>
2023-08-30 18:06:26 +0200

    counter game

--- app/src/main/java/com/m2i/tp2/MainActivity.java
+++ app/src/main/java/com/m2i/tp2/MainActivity.java
@@ -7,13 +7,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Random random = new Random(); // class random => generer nombres aleatoires ...
-    TextView nbr1_txt, nbr2_txt;
-    Button resp1_btn, resp2_btn, resp3_btn;
+    Timer michel = new Timer();
+    int result = 0, reponse;
+    boolean game= false;
+    TextView nbr1_txt, nbr2_txt,counter;
+    Button resp1_btn, resp2_btn, resp3_btn,start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +33,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         resp1_btn =findViewById(R.id.resp1);
         resp2_btn =findViewById(R.id.resp2);
         resp3_btn =findViewById(R.id.resp3);
+        counter = findViewById(R.id.counter);
+        start = findViewById(R.id.start);
 
         resp1_btn.setOnClickListener(this);
         resp2_btn.setOnClickListener(this);
         resp3_btn.setOnClickListener(this);
+        start.setOnClickListener(this);
     }
 
     public void start_game(){
@@ -42,27 +50,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int r2 = random.nextInt(400); // générer un nombre aleatoire entre 0 - 200
         int i = random.nextInt(3);
 
+        michel.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                result -= 1000;
+            }
+        }, 15*1000);
+
+
+        counter.setText("" + result);
         nbr1_txt.setText(String.valueOf(n1));
         nbr2_txt.setText(""+n2);
 
         if(i == 0){
             resp1_btn.setText(resp+"");
+            reponse = resp1_btn.getId();
             resp2_btn.setText(r1+"");
             resp3_btn.setText(r2+"");
         }else if (i == 1)
         {
             resp2_btn.setText(resp+"");
+            reponse = resp2_btn.getId();
             resp1_btn.setText(r1+"");
             resp3_btn.setText(r2+"");
         }else {
             resp3_btn.setText(resp+"");
+            reponse = resp3_btn.getId();
             resp2_btn.setText(r1+"");
             resp1_btn.setText(r2+"");
         }
+
+
     }
 
     @Override
     public void onClick(View v) {
+        if (v.getId() == start.getId()) {
+            if (game == false) {
+                game = true;
+                start_game();
+                start.setText("reset");
+                return;
+            }
+            else {
+                michel.purge();
+                result = 0;
+                start_game();
+                return;
+            }
+        }
+        if (game == false)
+            return;
+        if (v.getId() == reponse) {
+            result++;
+        }
+        else
+            result--;
+        michel.purge();
         start_game();
+        return;
     }
 }
