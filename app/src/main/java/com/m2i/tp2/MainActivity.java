d75b7f69669b45b9ae9ded92eccbdb5ff10f093d
Salim <bouzidsezlim@gmail.com>
2023-08-30 13:21:09 +0200

    fonction botton

--- app/src/main/java/com/m2i/tp2/MainActivity.java
+++ app/src/main/java/com/m2i/tp2/MainActivity.java
@@ -20,4 +20,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        nbr1_txt = findViewById(R.id.nbr1);
+        nbr2_txt = findViewById(R.id.nbr2);
+        resp1_btn =findViewById(R.id.resp1);
+        resp2_btn =findViewById(R.id.resp2);
+        resp3_btn =findViewById(R.id.resp3);
+
+        resp1_btn.setOnClickListener(this);
+        resp2_btn.setOnClickListener(this);
+        resp3_btn.setOnClickListener(this);
+    }
+
+    public void start_game(){
+        int n1 = random.nextInt(200); // générer un nombre aleatoire entre 0 - 200
+        int n2 = random.nextInt(200); // générer un nombre aleatoire entre 0 - 200
+        int resp = n1 + n2;
+        int r1 = random.nextInt(400); // générer un nombre aleatoire entre 0 - 200
+        int r2 = random.nextInt(400); // générer un nombre aleatoire entre 0 - 200
+        int i = random.nextInt(3);
+
+        nbr1_txt.setText(String.valueOf(n1));
+        nbr2_txt.setText(""+n2);
+
+        if(i == 0){
+            resp1_btn.setText(resp+"");
+            resp2_btn.setText(r1+"");
+            resp3_btn.setText(r2+"");
+        }else if (i == 1)
+        {
+            resp2_btn.setText(resp+"");
+            resp1_btn.setText(r1+"");
+            resp3_btn.setText(r2+"");
+        }else {
+            resp3_btn.setText(resp+"");
+            resp2_btn.setText(r1+"");
+            resp1_btn.setText(r2+"");
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        start_game();
+    }
 }
